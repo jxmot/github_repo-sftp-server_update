36298e3aacc33778d7fae5b5e22affd77d552624
jxmot <webdev@stupiddog.net>
2022-06-26 02:22:35 -0500

    added require of rightnow.php for getBackupPath()

--- modes.php
+++ modes.php
@@ -1,11 +1,13 @@
 <?php
+require_once 'rightnow.php';
+
 function stage($srcfile) {
     global $ghrepo, $sftp;
     if($ghrepo->getStage()->sourceroot !== '') {
         $srcfile = str_replace($ghrepo->getStage()->sourceroot, '', $srcfile);
     }
     return str_replace('%DOCROOT%', $sftp->getDocRoot(), $ghrepo->getStage()->dest) . $srcfile;
-};
+}
 
 function test($srcfile) {
     global $ghrepo, $sftp;
@@ -13,7 +15,7 @@ function test($srcfile) {
         $srcfile = str_replace($ghrepo->getTest()->sourceroot, '', $srcfile);
     }
     return str_replace('%DOCROOT%', $sftp->getDocRoot(), $ghrepo->getTest()->dest) . $srcfile;
-};
+}
 
 function live($srcfile) {
     global $ghrepo, $sftp;
