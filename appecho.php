acf382a36f214eb21f65908e24a2d893ca11ec3c
jxmot <webdev@stupiddog.net>
2022-06-27 09:22:03 -0500

    modified to use $runcfg->tstamp, if true then prepend timestamp to echo

--- appecho.php
+++ appecho.php
@@ -1,6 +1,10 @@
 <?php
 function appEcho($text) {
     global $runcfg;
-    if($runcfg->verbose === true) echo $text;
+    if($runcfg->verbose === true) {
+        if($runcfg->tstamp === true) {
+            echo rightnow('log') . " - " .$text;
+        } else echo $text;
+    }
 }
 ?>
