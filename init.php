b18aae0b7b8d615029f188333b7124c5ec239695
jxmot <webdev@stupiddog.net>
2022-07-01 07:48:37 -0500

    added Metrics.php, init Metrics class if enabled

--- init.php
+++ init.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once 'GitHubOwner.php';
 use serverupdate\GitHubOwner;
 $ghuser = new GitHubOwner($bpath, $runcfg->owner);
@@ -17,4 +16,20 @@ $sftp = new supdSFTP($bpath, $runcfg->server);
 
 appEcho("Home    - {$sftp->getHome()}\n");
 appEcho("DocRoot - {$sftp->getDocRoot()}\n");
+
+
+require_once 'Metrics.php';
+use serverupdate\Metrics;
+
+if($runcfg->metrics) {
+    $metrics = new Metrics($ghuser->GetOwner(), 
+                           $ghrepo->getName(),
+                           $sftp->getServer(),
+                           $runcfg->mode,
+                           $runcfg->debug);
+
+    appEcho("metrics file - " . $metrics->getFileName() . "\n");
+} else {
+    $metrics = null;
+}
 ?>
